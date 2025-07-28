dc489cdd220c78b5ecff53eecafa122a42439365
Alejandro Albarran <115290483+AAlejandro8@users.noreply.github.com>
2025-07-28 13:47:57 -0400

    Switch to PyJWT for better deployment compatibility

--- app/utils/auth.py
+++ app/utils/auth.py
@@ -1,5 +1,6 @@
 from fastapi import Depends, status, HTTPException
-from jose import JWTError, jwt
+import jwt
+from jwt import InvalidTokenError as JWTError
 from datetime import datetime, timedelta, timezone
 from fastapi.security import OAuth2PasswordBearer
 from sqlalchemy.orm import Session
@@ -19,27 +20,20 @@ ACCESS_TOKEN_EXPIRE_MINUTES = 60
 
 def create_access_token(data: dict):
     to_encode = data.copy()
-    
-    expire = datetime.now(timezone.utc) + timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
+    expire = datetime.utcnow() + timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
     to_encode.update({"exp": expire})
-
     encoded_jwt = jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
-
     return encoded_jwt
 
-def verify_access_token(token: str, credentials_exception):
+def verify_token(token: str, credentials_exception):
     try:
         payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
-
-        id: str = payload.get("user_id")
-
-        if id is None:
+        username: str = payload.get("sub")
+        if username is None:
             raise credentials_exception
-        token_data = schemas.TokenData(id=str(id))
+        return username
     except JWTError:
         raise credentials_exception
-    
-    return token_data
 
 def get_current_user(token: str = Depends(oauth2_scheme), db: Session = Depends(get_db)):
     credentials_exception = HTTPException(
@@ -48,7 +42,7 @@ def get_current_user(token: str = Depends(oauth2_scheme), db: Session = Depends(
         headers={"WWW-Authenticate": "Bearer"}
     )
     
-    token = verify_access_token(token, credentials_exception)
+    token = verify_token(token, credentials_exception)
 
     user = db.query(models.Employee).filter(models.Employee.public_id == token.id).first()
 
